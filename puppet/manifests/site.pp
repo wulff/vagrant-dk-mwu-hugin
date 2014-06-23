afb01fa656687ca0ec27351593cb5dfe34d63d11
wulff <wulff@ratatosk.net>
2014-06-23 20:35:34 +0200

    Fixed syntax error

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -155,7 +155,6 @@ class hugin::install {
     content => 'alias update="sudo apt-get update"
 alias upgrade="sudo apt-get upgrade"
 alias puppet-apply="sudo puppet apply --modulepath=/home/wulff/vagrant/puppet/modules/ /home/wulff/vagrant/puppet/manifests/site.pp"',
-    source  => 'puppet:///modules/dotfiles/aliases.sh',
     mode => 0644,
   }
 }
